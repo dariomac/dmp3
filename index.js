c47a7c18b0546b3cd9b505665887b6d0387795a4
Gabriel Rodriguez <grodriguez@kubikware.com>
2020-06-05 16:56:49 -0300

    First playable version. Only play and nothing more, but do the work :-)

--- index.js
+++ index.js
@@ -1,27 +1,54 @@
-
-/*
-const playSong = require('./play-song');
-
-const dirs = process.argv.slice(2);
-console.log(dirs)
-
-playSong({
-  //path: './mp3/01. Darker Thoughts.mp3'
-  path: '/Volumes/MP3/Blues/Hugh Laurie - Let Them Talk (2011)/01 - St. James Infirmary.mp3'
-});
-*/
 const commandLineArgs = require('command-line-args');
+const player = require('./lib/player');
 
 const optionDefinitions = [
   { name: 'mode', alias: 'm', type: String },
-  { name: 'src', type: String, multiple: true, defaultOption: true },
+  { name: 'src', type: String, multiple: false, defaultOption: true },
+  { name: 'simulate', alias: 's', type: Boolean },
 ];
 
 const opts = commandLineArgs(optionDefinitions);
 
-if (! opts.src) {
-  console.log('Yo must specify at least one path');
-}
-else {
-  console.log(opts.src);
-}
+(async () => {
+  if (! opts.src) {
+    console.log('You must specify at least one path');
+  }
+  else {
+
+    console.log(opts)
+    if (!opts.simulate) {
+      await player.init(opts);
+    }
+    else {
+      let playState = await player.getNext({
+        playing: opts.src
+      });
+
+      console.log(`Play this song: ${playState.playing}`);
+      console.log(`Next song: ${playState.next}\n`);
+      
+      const i = setInterval(async function(){
+        playState.playing = playState.next;
+        playState = await player.getNext(playState);
+        
+        if (!playState) {
+          console.log('END PLAYING');
+          clearInterval(i);
+          return;
+        }
+
+        console.log(`Play this song: ${playState.playing}`);
+        console.log(`Next song: ${playState.next}\n`);
+
+        if (!playState.next) {
+          console.log('END PLAYING');
+          clearInterval(i);
+          return;
+        }
+      }, 250);
+    }
+  }
+})().catch(e => {
+  console.log(e)
+  // Deal with the fact the chain failed
+});
