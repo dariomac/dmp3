7259f8d48edcae0113ec31c7bb271f427504d6fb
Dario Macchi <dariomac@gmail.com>
2025-01-08 08:40:00 -0300

    Big MPV refactor

--- cli.js
+++ cli.js
@@ -1,26 +1,24 @@
 #!/usr/bin/env node
 import commandLineArgs from 'command-line-args';
 
-import { player } from './lib/player.js';
+import * as player from './lib/player.js';
 import { keyboardListener } from './lib/keyboardListener.js';
-import printer from './lib/printer.js';
+import * as printer from './lib/printer.js';
 import simulate from './lib/simulate.js';
 
-printer.printClear();
-printer.printMsg('version: 1.0.2\n');
-
-const optionDefinitions = [
+const opts = commandLineArgs([
   { name: 'mode', alias: 'm', type: String },
   { name: 'src', type: String, multiple: false, defaultOption: true },
   { name: 'simulate', alias: 's', type: Boolean },
   { name: 'loglevel', alias: 'l', type: Number, defaultValue: -1 },
-];
-
-const opts = commandLineArgs(optionDefinitions);
-printer.setLogLevel(opts.loglevel);
+]);
 
 (async () => {
-  if (! opts.src) {
+  printer.setLogLevel(opts.loglevel);
+  printer.printClear();
+  printer.printMsg('version: 1.0.2\n\n');
+  
+  if (!opts.src) {
     printer.printErr('You must specify at least one path');
     process.exit(1);
   }
@@ -30,18 +28,11 @@ printer.setLogLevel(opts.loglevel);
     process.exit(0);
   }
 
-  printer.printMsg('\n');
-
   await player.init(opts);
   await keyboardListener.init(player);
 
   setTimeout(async () => {
-    try {
-      await player.play();
-    } catch (e) {
-      printer.printErr(e);
-      process.exit(1);
-    }
+    await player.play();
   }, 500);
 
 })().catch(e => {
